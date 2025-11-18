1b795257ca3faea529673ac41a5cd4f09bb188b9
Owen Bellish <owenbellish@gmail.com>
2025-11-18 14:58:47 -0500

    missed one

--- src/color.rs
+++ src/color.rs
@@ -17,13 +17,9 @@ pub fn write_color<W: Write>(
 	let result = (scale * simd).sqrt();
 	let clamped = result.simd_clamp(f64x4::splat(0.0), f64x4::splat(0.999));
 
-	let [r, g, b, _] = clamped.to_array();
+	let [r, g, b, _] = (clamped * f64x4::splat(256.0)).to_array();
 
-	writeln!(
-		out,
-		"{} {} {}",
-		(256.0 * r) as i32,
-		(256.0 * g) as i32,
-		(256.0 * b) as i32,
-	)
+	writeln!(out, "{} {} {}", r as i32, g as i32, b as i32)?;
+
+	Ok(())
 }
