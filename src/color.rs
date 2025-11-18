4b9766b91d802fef4ff9b29b5be78fbf370ef282
Owen Bellish <owenbellish@gmail.com>
2025-11-18 14:46:18 -0500

    more simd

--- src/color.rs
+++ src/color.rs
@@ -1,27 +1,29 @@
-use std::io::{Result as IoResult, prelude::*};
+use std::{
+	io::{Result as IoResult, prelude::*},
+	simd::{StdFloat as _, prelude::*},
+};
 
-use super::{Vec3, utils};
+use super::Vec3;
 
 pub fn write_color<W: Write>(
 	out: &mut W,
 	pixel_color: Vec3,
 	samples_per_pixel: i32,
 ) -> IoResult<()> {
-	let mut r = pixel_color.x();
-	let mut g = pixel_color.y();
-	let mut b = pixel_color.z();
+	let simd = pixel_color.to_simd();
 
-	let scale = 1.0 / f64::from(samples_per_pixel);
+	let scale = f64x4::splat(1.0 / f64::from(samples_per_pixel));
 
-	r = f64::sqrt(scale * r);
-	g = f64::sqrt(scale * g);
-	b = f64::sqrt(scale * b);
+	let result = (scale * simd).sqrt();
+	let clamped = result.simd_clamp(f64x4::splat(0.0), f64x4::splat(0.999));
+
+	let [r, g, b, _] = clamped.to_array();
 
 	writeln!(
 		out,
 		"{} {} {}",
-		(256.0 * utils::clamp(r, 0.0, 0.999)) as i32,
-		(256.0 * utils::clamp(g, 0.0, 0.999)) as i32,
-		(256.0 * utils::clamp(b, 0.0, 0.999))
+		(256.0 * r) as i32,
+		(256.0 * g) as i32,
+		(256.0 * b) as i32,
 	)
 }
